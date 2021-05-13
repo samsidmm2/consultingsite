0c733de322d269e01f6154e890c8341759d8c263
Sidney Sykes <47064793+samsidmm2@users.noreply.github.com>
2021-05-13 00:03:12 -0500

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -12,3 +12,60 @@ function sendEmail() {
         message => alert("mail send successfully")
     );
 }
+
+function error_message(id, message) {
+    document.getElementById(id).innerText = message;
+}
+
+function validateContact() {
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+    var email = document.getElementById("email");
+    var message = document.getElementById("message");
+
+    var firstnameValue = firstname.value;
+    var lastnameValue = lastname.value;
+    var emailValue = email.value;
+    var messageValue = message.value;
+
+
+
+    if (firstnameValue == "") {
+        proEmail.style.border = "1px solid #ff8471";
+        error_message("fname_err_msg", "This field is required and cannot be empty.");
+
+    }
+
+    if (lastnameValue == "") {
+        proEmail.style.border = "1px solid #ff8471";
+        error_message("lname_err_msg", "This field is required and cannot be empty.");
+
+    }
+
+
+
+    if (emailValue == "") {
+        proEmail.style.border = "1px solid #ff8471";
+        error_message("email_err_msg", "This field is required and cannot be empty.");
+
+    }
+
+    if (messageValue == "") {
+        password.style.border = "1px solid #ff8471";
+        error_message("msg_err_msg", "This field is required and cannot be empty.");
+
+    }
+
+
+    if (firstnameValue == "" || lastnameValue == "" || emailValue == "" || messageValue == "") {
+        alert("Please fill this out. This is required");
+        return false;
+    } else {
+        return true;
+    }
+
+
+
+    // alert(firstnameValue + " " + lastnameValue + " " + proEmailValue + " " + schoolEmailValue + " " + passwordValue + " " + password2Value);
+
+}
